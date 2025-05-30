2478d7e02d077cb8e65fe02f792518040bc9225e
AyoubMzian-Dev <ayoubmziandeveloper@gmail.com>
2025-05-30 13:19:36 +0100

    update subject api

--- app/api/subjects/route.ts
+++ app/api/subjects/route.ts
@@ -6,8 +6,13 @@ export async function GET() {
   try {
     await connectDB();
     const subjects = await Subjects.find().lean();
+    if(subjects.length == 0){
+      console.log("No subjects found");
+      return NextResponse.json({ error: "No subjects found" }, { status: 404 });
+    }
     return NextResponse.json(subjects, { status: 200 });
-  } catch {
-    return NextResponse.json({ error: "Failed to fetch subjects" }, { status: 500 });
+
+  } catch (error) {
+    return NextResponse.json({ error: "Failed to fetch subjects" + error }, { status: 500 });
   }
 }
